8eb76999a36d245b0d19b38cf06caf9a48dc9b21
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-03-01 16:33:34 +0600

    doing JS

---
+++ script/btn-operation.js
@@ -0,0 +1,7 @@
+// document.getElementById('mobile-btn')
+// .addEventListener('click', function(event){
+//     event.preventDefault();
+//     document.getElementById('mobile-btn').setAttribute('disabled', true);
+// })
+
+const btn = document.getElementById('mobile-btn').setAttribute('disabled', true);
